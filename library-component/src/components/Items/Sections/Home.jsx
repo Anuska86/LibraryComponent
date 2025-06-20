b2f18fed6cdeb6d89e37c3655e54cef3ebabb86b
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-20 12:25:00 +0200

    npm

--- library-component/src/components/Items/Sections/Home.jsx
+++ library-component/src/components/Items/Sections/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Menu from "../../Menu/MenuIndex";
+import Button from "../../Button/Button";
 
 export default function Home() {
+  function handleCopy() {
+    navigator.clipboard.writeText("npm install anuska-library");
+  }
+
   return (
     <div className="home-container">
       <h1 className="title">Welcome to the Library Component.</h1>
@@ -10,45 +15,75 @@ export default function Home() {
         This library provides reusable UI components with predefined styles for
         badges, banners, cards, and testimonials. Designed for flexibility, it
         allows developers to easily integrate these elements into their
-        applications.
+        applications.You can use npm to install the library and start using
+        these components in your projects. Thank you for choosing Anuska's
+        Library! 😊
       </p>
-      <div>
-        <Menu onOpen={() => console.log("Menu opened/closed")}>
-          <Menu.Button>Menu</Menu.Button>
-          <Menu.Dropdown>
-            <Menu.Item>
-              <Link className="link-button" to="/badges">
-                Badges
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              {" "}
-              <Link className="link-button" to="/banners">
-                Banners
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link className="link-button" to="/cards">
-                Cards
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link className="link-button" to="/testimonials">
-                Testimonials
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link className="link-button" to="/tooltips">
-                Tooltips
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link className="link-button" to="/toast-popups">
-                Toast Popups
-              </Link>
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
+      <div className="home-flex">
+        <div className="home-menu">
+          <Menu>
+            <Menu.Button>Menu</Menu.Button>
+            <Menu.Dropdown>
+              <Menu.Item>
+                <Link className="link-button" to="/badges">
+                  Badges
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                {" "}
+                <Link className="link-button" to="/banners">
+                  Banners
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link className="link-button" to="/cards">
+                  Cards
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link className="link-button" to="/testimonials">
+                  Testimonials
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link className="link-button" to="/tooltips">
+                  Tooltips
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link className="link-button" to="/toast-popups">
+                  Toast Popups
+                </Link>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
+
+        <section className="installation-section">
+          <h2>Get Started</h2>
+          <p>Install the library using npm:</p>
+          <div className="install-command">
+            <code>npm install anuska-library</code>
+            <Button
+              className="copy-button"
+              variant="secondary"
+              size="lg"
+              arial-controls="copyButton"
+              onClick={handleCopy}
+            >
+              Copy
+            </Button>
+          </div>
+          <p>
+            <a
+              href="https:www.npmjsx.package/anuska-library"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View on npm
+            </a>
+          </p>
+        </section>
       </div>
     </div>
   );
