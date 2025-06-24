469cb9321d389133eb224a84cc76b5613aa1a214
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-24 11:10:11 +0200

    Update

--- demo/src/components/Menu/MenuDropdown.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MenuContext } from "./Menu.jsx";
-
-export default function MenuDropdown({ children }) {
-  const { open, toggleOpen } = React.useContext(MenuContext);
-
-  return <>{open ? <div className="menu-dropdown">{children}</div> : null}</>;
-}
