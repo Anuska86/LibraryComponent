536f91c1d1c62ec6dd1454589f9b87a5e72857e8
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-06-24 11:16:45 +0200

    Update

--- library-component/vite.config.js
+++ library-component/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]/*,
   build: {
     lib: {
       entry: "src/index.js",
@@ -21,5 +21,5 @@ export default defineConfig({
         },
       },
     },
-  },
+  },*/
 });
